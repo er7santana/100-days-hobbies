188f487e5547cba01f6a7d62a9d0e1cd6d21538a
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-07-29 07:01:08 -0300

    Initial Commit

---
+++ Hobbies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hobbies
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 29/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hobbies/HobbiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HobbiesApp.swift
+//  Hobbies
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct HobbiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
